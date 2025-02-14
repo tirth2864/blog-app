30d863689f81ffd0b574842b5ee56109369aa112
tirth2864 <tirth2864@gmail.com>
2025-02-14 16:25:58 +0530

    Add : authRoutes

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+
+router.post("./register", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const user = new User({ username, email, password });
+    await user.save();
+    res.status(201).json({ message: "User Registered Succesfully!" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+    res.status(200).json({ message: "Login Successful" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+module.exports = router;
